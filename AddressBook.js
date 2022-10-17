bf5dcba5e8913399b81ddaa29446b6a2c3eb9dfd
Tarun <113088734+tarun-veerabrahma@users.noreply.github.com>
2022-10-18 02:38:40 +0530

    Refining the code
    Also added individual field validation
    Cancel functionality
    updated edit and delete functionalities.

--- AddressBook.js
+++ AddressBook.js
@@ -8,103 +8,138 @@ var contactTileLayout = `<div class="contactTile">
 						<div><span class="mobile"></span></div>
 					</div>
 				</div>`;
-
 var validationPatterns = {
 	"name": "^[A-za-z ]{3,}$",
 	"email": "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,3}$",
 	"mobile": "^[(]?[\+]?[0-9]{0,3}[)]?( )?[0-9]{10}$",
-	"landline": "^[0-9]{2,4}[- ]?[0-9]{6,8}$",
+	"landline": "^[0-9]{2,4}[(\-) ]?[0-9]{6,8}$",
 	"website": "^((http|https):\/\/)?([w]{3}.)?[A-Za-z]+([A-Za-z0-9_(\-)%:\.\/])+$",
-	"address": "[0-9A-Za-z\.(\-)\/: \n]*$"
+	"address": "[0-9A-Za-z\.(\-)\/: \n]+$"
+}
+var validationFlag=0;
+
+
+
+
+function showForm(elementId){
+	$("#formContainer").find(".buttons").addClass("hide");
+	if(elementId=="addContact"){
+		$("#addContactButtonsContainer").removeClass("hide");
+	}
+	if(elementId=="editOption"){
+		$("#editContactButtonsContainer").removeClass("hide");
+	}
+	$("#formContainer").removeClass("hide");
+}
+
+function cancelForm(){
+	$("#formContainer").addClass("hide");
+	$("#contactForm").trigger("reset");
+	resetValidationMsg("contactForm");
 }
 
 
-function formValidation(This){
-	let fields = $("#"+This).find(":input");
-	let values = fields.serializeArray();
-	console.log(fields[0].value);
-	console.log(values);
-	let count=0;
-	fields.each(function(index){
-		let input = $(this)[0].value;
-		let name=$(this)[0].name;
-		let ele = $(this)[0];
-		let label = $(ele).parent().find(".label").text();
+function fieldValidation(element){
+	console.log(element);
+	let input = $(element)[0].value;
+	let name=$(element)[0].name;
+	let temp = $(element)[0];
+	let label = $(temp).parent().find(".formLabel").text();
+	if(!(input.match(validationPatterns[name]))){
+		validationFlag = 0;
 		if(input==""){
-			$(ele).parent().find(".validationMsg").text(label+" is required");
+			$(temp).parent().find(".validationMsg").text(label+" is required");
 		}
 		else{
-			if(!(input.match(validationPatterns[name]))){
-				$(ele).parent().find(".validationMsg").text("Invalid "+name);
-			}
-			else{
-				$(ele).parent().find(".validationMsg").text("");
-				count++;
-			}
+			$(temp).parent().find(".validationMsg").text("Invalid "+label);
 		}
+	}
+	else{
+		$(temp).parent().find(".validationMsg").text("");
+	}
+}
 
+function resetValidationMsg(elementId){
+	$("#"+elementId).find(".validationMsg").each(function(){
+		$(this).text("");
 	})
-	if(fields.length == count){
+}
+
+
+function setContactObj(values){
+	contactObj = {};
+	for(let i=0;i<values.length;i++){
+		let key = values[i].name;
+		contactObj[key] = values[i].value;
+	}
+}
+
+function setContactTileValues(contactTile){
+	for(var key in contactObj){
+		if(contactTile.find("."+key).length != 0){
+			contactTile.find("."+key).text(contactObj[key]);
+		}
+	}
+}
+
+function setContactDetailsSectionValues(id){
+	contactObj = JSON.parse(contacts[id]);
+	for(var key in contactObj){
+		if($("#"+key).length !=0){
+			$("#"+key).text(contactObj[key]);
+		}
+	}
+	$("#contactDetailsSection.hide").removeClass("hide");
+}
+
+function setInputFieldsValues(id){
+	let values = $("#contactForm :input");
+	contactObj = JSON.parse(contacts[id]);
+	let temp = Object.values(contactObj);
+	for(let i=0;i<temp.length;i++){
+		values[i].value = temp[i];
+	}
+}
+
+
+
+function formValidation(elementId){
+	let fields = $("#"+elementId).find(":input");
+	let values = fields.serializeArray();
+	validationFlag=1;
+	fields.each(function(){
+		fieldValidation(this);
+	})
+	if(validationFlag){
 		return true;
 	}
 	else{ return false;}
 
 }
 
-function changePage(This){
-	console.log(This);
+function changePage(elementId){
 	$("#navigationContainer").find(".active").removeClass("active");
-	$("#"+This).addClass("active");
-	if(This == "addContact")
+	$("#"+elementId).addClass("active");
+	if(elementId == "addContact")
 	{
-		unhideForm(This);
+		showForm(elementId);
 	}
 	else{
-		hideForm();	
+		cancelForm();	
 	}
 }
-
-function unhideForm(This){
-	if(This=="addContact"){
-		//$("#navigationContainer, #contactsSection").addClass("disable");
-		$("#formContainer").find(".buttons").addClass("hide");
-		$("#addContactSectionButtons").removeClass("hide");
-		$("#formContainer").removeClass("hide");
-	}
-	if(This=="editOption"){
-		//$("#navigationContainer, #contactsSection").addClass("disable");
-		$("#formContainer").find(".buttons").addClass("hide");
-		$("#editContactSectionButtons").removeClass("hide");
-		$("#formContainer").removeClass("hide");
-	}
-}
-
-function hideForm(){
-	$("#formContainer").addClass("hide");
-	$("#contactDetailsForm").trigger("reset");
-	//$("#navigationContainer, #contactsSection").removeClass("disable");
-}
-
-
 
 function addContact(){
-	var values = $("#contactDetailsForm :input").serializeArray();
-	if(formValidation("contactDetailsForm")){
-		console.log("approved");
-		for(let i=0;i<values.length;i++){
-			let key = values[i].name;
-			contactObj[key] = values[i].value;
-		}
-		let t=$(contactTileLayout).appendTo("#contactsSection");
-		t.find(".name").html(contactObj.name);
-		t.find(".email").html(contactObj.email);
-		t.find(".mobile").html(contactObj.mobile);
-		let temp = t[0];
+	var values = $("#contactForm :input").serializeArray();
+	if(formValidation("contactForm")){
+		setContactObj(values);
+		let newContactTile=$(contactTileLayout).appendTo("#contactsSection");
+		setContactTileValues(newContactTile);
+		let temp = newContactTile[0];
 		temp.setAttribute("id",++uniqueId);
-		let id=uniqueId;
-		let elements = $("#"+String(id)).find("span");
 		contacts[uniqueId]=JSON.stringify(contactObj);
-		hideForm();
+		newContactTile.click();
+		changePage("home");
 	}
 		
 }
@@ -112,26 +147,14 @@ function addContact(){
 $("#contactsSection").on("click",".contactTile",function(){
 	$(".contactTile.active").removeClass("active");
 	$(this).addClass("active");
-	let key = $(this).attr("id");
-	let temp = JSON.parse(contacts[key]);
-	$("#name").html(temp.name);
-	$("#email").html(temp.email);
-	$("#mobile").html(temp.mobile);
-	$("#landline").html(temp.landline);
-	$("#website").html(temp.website);
-	$("#addressValue").html(temp.address);
-	$("#contactDetailsSection.hide").removeClass("hide");
+	let id = $(this).attr("id");
+	setContactDetailsSectionValues(id);
 })
 
-function editDetails(This){
+function editContact(element){
 	let id = $(".contactTile.active")[0].getAttribute("id");
-	let values = $("#contactDetailsForm :input");
-	let object = JSON.parse(contacts[id]);
-	let temp = Object.values(object);
-	for(let i=0;i<temp.length;i++){
-		values[i].value = temp[i];
-	}
-	unhideForm(This);
+	setInputFieldsValues(id);
+	showForm(element);
 }
 
 function deleteContact(){
@@ -139,29 +162,19 @@ function deleteContact(){
 	$(".contactTile.active")[0].remove();
 	delete contacts[id];
 	$("#contactDetailsSection").addClass("hide");
+	$("#contactsSection").find(".contactTile")[0].click();
 
 }
 
-function updateDetails(){
-	let id = $(".contactTile.active")[0].getAttribute("id");
-	let values = $("#contactDetailsForm :input").serializeArray();
-	
-	let contactObj={};
-	for(let i=0;i<values.length;i++){
-		let key = values[i].name;
-		contactObj[key] = values[i].value;
+function updateContact(){
+	if(formValidation("contactForm")){
+		let id = $(".contactTile.active")[0].getAttribute("id");
+		let values = $("#contactForm :input").serializeArray();
+		setContactObj(values);
+		contacts[id] = JSON.stringify(contactObj);
+		cancelForm();
+		let contactTile = $(".contactTile.active");
+		setContactTileValues(contactTile);
+		contactTile.click();
 	}
-	contacts[id] = JSON.stringify(contactObj);
-	hideForm();
-	let temp = contactObj;
-	let t = $(".contactTile.active");
-	t.find(".name").html(contactObj.name);
-	t.find(".email").html(contactObj.email);
-	t.find(".mobile").html(contactObj.mobile);
-	$("#name").html(temp.name);
-	$("#email").html(temp.email);
-	$("#mobile").html(temp.mobile);
-	$("#landline").html(temp.landline);
-	$("#website").html(temp.website);
-	$("#addrValue").html(temp.address);
 }
